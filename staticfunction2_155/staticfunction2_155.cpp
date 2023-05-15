c87142468dbbc832c262048136b56575ae63c694
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-05-15 15:48:58 +0700

    Membuat class variabel dengan menggunakan member static function

---
+++ staticfunction2_155/staticfunction2_155.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setId();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; /*definisi function*/ }
+	static int getNim() { return nim; /*Definisi function*/ }
+
+	mahasiswa(string pnama) : nama(pnama) { setId(); }
+};
