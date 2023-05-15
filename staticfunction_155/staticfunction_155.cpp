1d8586b8a835fbc43356c54738ecf508c5834db7
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-05-15 15:28:45 +0700

    Add project files.

---
+++ staticfunction_155/staticfunction_155.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setId();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setId(); }
+};
+
