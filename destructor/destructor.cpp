71bbfbde92627aca21df096111a9d59f308e8fbf
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-05-15 18:56:50 +0700

    Membuat destructor

---
+++ destructor/destructor.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class angka {
+private:
+	int *arr;
+	int panjang;
+public:
+	angka(int);		//constructor
+	~angka();		//destructor
+	void cetakData();
+	void isiData();
+};
+
+//definisi member function
+angka::angka(int i) {		//consturtor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() {
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array sudah dilepaskan" << endl;
+}
+
+void angka::cetakData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3);		//constructor dipanggil
+	angka *ptrbelajarcpp = new angka(5);		//constructor dipanggil
+	delete ptrbelajarcpp;		//destructor dipanggil
+
+	return 0;
+}		// destructor dipanggil
